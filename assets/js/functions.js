3154b57c2790d8f0df5545db8578c73c7e989f96
LeNPaul <l.nguyen.paul@gmail.com>
2017-05-07 01:49:19 -0400

    Improve problem with side page scrolling

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,11 +6,11 @@ function workBelt() {
   $('.thumb-unit').click(function() {
     $('.work-belt').css('left','-100%');
     $('.work-container').show();
-    //$('.thumb-container').hide(800);
+    $('.thumb-unit').hide(800);
   });
   $('.work-return').click(function() {
     $('.work-belt').css('left','0%');
     $('.work-container').hide(800);
-    //$('.thumb-container').show();
+    $('.thumb-unit').show();
   });
 }
