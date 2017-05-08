d040878a41d260d75613cb3108b8d7719312d5bf
LeNPaul <l.nguyen.paul@gmail.com>
2017-05-08 00:01:21 -0400

    Add project content injection

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
   workBelt();
+  workLoad();
 });
 
 function workBelt() {
@@ -12,5 +13,18 @@ function workBelt() {
     $('.work-belt').css('left','0%');
     $('.work-container').hide(800);
     $('.thumb-unit').show();
+  });
+}
+
+function workLoad() {
+  $.ajaxSetup ({ cache: true });
+  $('.thumb-unit').click(function() {
+    var $this = $(this),
+    newTitle = $this.find('strong').text(),
+    newFolder = $this.data('folder'),
+    spinner = '<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i><span class="sr-only">Loading...</span>',
+    newHTML = '/work/' + newFolder + '.html';
+    $('.project-load').html(spinner).load(newHTML);
+    $('.project-title').text(newTitle);
   });
 }
